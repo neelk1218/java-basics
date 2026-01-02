d11143c55e897f20ee6a26228eac2d220a631a0d
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:21:41 +0530

    Create userInput.java

---
+++ Basic  Scanners /userInput.java
@@ -0,0 +1,14 @@
+//Taking input in java
+
+
+import java.util.Scanner;
+
+public class userInput {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    String name = sc.nextLine();
+
+    System.out.println("Name" +name);
+  }
+}

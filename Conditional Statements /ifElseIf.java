74febf86b2b10f23afc26d0cef5c286743935f30
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:18:39 +0530

    Add if-else statements to find the largest number
    Implement conditional statements to compare four numbers and print the largest.

---
+++ Conditional Statements /ifElseIf.java
@@ -0,0 +1,31 @@
+//exaple:- Comapring 4 numbers
+import java.util.Scanner;
+public class Main{
+  public static void main(String[]args){
+    int a = 8;
+    int b = 21;
+    int c = 3;
+    int d = 20;
+
+    
+
+    if(a>b && a>c && a>d)
+     {
+      System.out.print(a);
+
+     }
+    else if(b >c && b>d)
+     {
+      System.out.print(b);
+    
+     } 
+    else if(c>d)
+    {
+      System.out.print(c);
+    }
+    else
+    {
+      System.out.print(d);
+    }
+  }
+}

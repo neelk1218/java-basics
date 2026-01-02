7ae503154a48170e219df19a2e618af26fb1516c
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:02:44 +0530

    Update and rename Operators to Operator/arithemeticOperators.java

---
+++ Operator/arithemeticOperators.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 6;
+
+        // Arithmetic Operators
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int modulus = a % b;
+
+        // Output
+        System.out.println("Addition       : " + addition);
+        System.out.println("Subtraction    : " + subtraction);
+        System.out.println("Multiplication : " + multiplication);
+        System.out.println("Division       : " + division);
+        System.out.println("Modulus        : " + modulus);
+    }
+}

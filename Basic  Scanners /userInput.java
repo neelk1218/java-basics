2a372ca10f66663f8f55e2a942ec235f78272b7e
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:23:32 +0530

    Rename class Main to userInput

--- Basic  Scanners /userInput.java
+++ Basic  Scanners /userInput.java
@@ -16,7 +16,7 @@ public class userInput {
 //EXAMPLE//
 // sum of double vvariables 
 import java.util.Scanner;
-public class Main{
+public class userInput{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     double num1 = sc.nextDouble();

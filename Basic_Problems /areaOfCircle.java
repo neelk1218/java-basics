e56b10b2084fe8c806fbc156b8e49260cbf7f2de
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:34:48 +0530

    Rename class Main to areaOfCircle

--- Basic_Problems /areaOfCircle.java
+++ Basic_Problems /areaOfCircle.java
@@ -1,7 +1,7 @@
 
 //find Area of circle 
 import java.util.Scanner;
-public class Main{
+public class areaOfCircle{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     

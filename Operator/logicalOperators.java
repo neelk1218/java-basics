bea69259f7861c2a32993a8bbacc997ffce88faa
Neel <nkhamkar2006@gmail.com>
2026-01-02 12:11:21 +0530

    Add logical operators demonstration in Java

---
+++ Operator/logicalOperators.java
@@ -0,0 +1,12 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        // Logical Operators
+        System.out.println("a && b : " + (a && b));   // AND
+        System.out.println("a || b : " + (a || b));   // OR
+        System.out.println("!a     : " + (!a));       // NOT
+    }
+}

309d26f558c320d37d92fba33292c645851de42d
Neel <nkhamkar2006@gmail.com>
2026-01-02 11:35:21 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,15 @@
+
+// Variavles in Java
+public class Main{
+    public static void main(String[] args){
+        String name = "Neel";
+        int age = 19;
+        String city = "Mumbai";
+        double salary = 80000.50;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+        System.out.println(salary);    
+    }
+}

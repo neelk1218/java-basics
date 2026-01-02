051db8f81fd4714e4e8676b7cd87942dc870859a
Neel <nkhamkar2006@gmail.com>
2026-01-02 11:36:56 +0530

    Update variables.java

--- variables.java
+++ variables.java
@@ -13,3 +13,28 @@ public class Main{
         System.out.println(salary);    
     }
 }
+
+public class Main{
+  public static void main(String[]args){
+    int num1 = 2;
+    int num2 = 4;
+    int result = num1 + num2;
+    System.out.println("Result =" + result);
+  }
+}
+
+//printing variables 
+public class Main{
+  public static void main(String[]args){
+    String name = " Neel";
+    int age = 19;
+    int salary = 100000;
+    boolean isMarried =  true;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(salary);
+    System.out.println(isMarried);
+  }
+}
+

3cfc389ccefa1d69f8462fbda595d9dfb34da332
Neel <nkhamkar2006@gmail.com>
2026-01-04 20:35:52 +0530

    Add while loop example in whileLoop.java

---
+++ Loops/whileLoop.java
@@ -0,0 +1,14 @@
+//LOOPS
+//WHILE LOOP 
+public class Main{
+  public static void main(String[]args){
+   
+    int i = 1 ;
+
+    while(i<=5){
+      System.out.println("HELLO WOLRD" + i );
+      i++;
+    }
+    
+  }
+}

49ad3cea1648842b9b56f793320e8ab6df1696af
Neel <nkhamkar2006@gmail.com>
2026-01-04 20:28:32 +0530

    Implement switch case for day of the week

---
+++ Conditional Statements /switchCase.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class switchCase{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+
+    //Taking user INPUT 
+    int day = sc.nextInt();
+
+    //switch Statement
+    switch(day){
+      case 1:
+        {
+          System.out.println("Monday");
+          break;
+        }
+      case 2:
+        {
+          System.out.println("Tuesday");
+          break;
+        }
+      case 3:
+        {
+          System.out.println("Wednesday");
+          break;
+        }
+      case 4:
+        {
+          System.out.println("Thrusday");
+          break;
+        }
+      case 5:
+        {
+          System.out.println("Friday");
+          break;
+        }
+      case 6: 
+      {
+        System.out.println("Saturday");
+        break;
+      }
+      case 7:
+        {
+          System.out.println("Sunday");
+          break;
+        }
+      default:
+        {
+          System.out.println("Error , Please Enter A Valid Number");
+        }
+    }
+
+    
+  }
+}

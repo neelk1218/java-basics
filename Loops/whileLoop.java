09e14da0ebbbeddbbb6a59a792a68326f92994aa
Neel <nkhamkar2006@gmail.com>
2026-01-04 20:42:43 +0530

    Implement nested while loop for greetings
    Added a nested while loop to print greetings.

--- Loops/whileLoop.java
+++ Loops/whileLoop.java
@@ -12,3 +12,23 @@ public class Main{
     
   }
 }
+
+//NESTED WHILE LOOP 
+public class whileLoop{
+  public static void main(String[]args){
+    int i = 1;
+
+    while(i<=5){
+      System.out.println("Good Morning" + i);
+
+      int j =1;
+      while(j<=5){
+        System.out.println("Hello World" + j);
+        j++;
+      }
+      i++;
+
+    }
+  }
+}
+

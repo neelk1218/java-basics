ee1ef88abb4c863a178a422f34660e1ed96f8eb4
Neel <nkhamkar2006@gmail.com>
2026-01-06 00:39:44 +0530

    Uncomment code to find the largest of three numbers

---
+++ Basic_Problems /largestOfThree.java
@@ -0,0 +1,24 @@
+/ //largest of 3 numbers
+public class Main{
+  public static void main(String[]args){
+
+    int a = 18;
+    int b = 5;
+    int c = 9;
+
+
+    //comparing 3 numbers
+    if(a>b && a>b && a>c)
+     {
+      System.out.println( a + "is the Greatest Number");
+     }
+    else if(b>c)
+    {
+      System.out.println( b + "is the Greatest Number");
+    }
+    else
+    {
+      System.out.println( c +" is the Greatest Number");
+    }
+  }
+}
